fb926428769d865c2f1eaa60718116870957272a
MedovTimur <62596970+MedovTimur@users.noreply.github.com>
2020-05-23 15:41:44 +0300

    Add files via upload

--- pdp.c
+++ pdp.c
@@ -1,61 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
 #include "headers.h"
+#include <string.h>
 
-
-
-
-
-/*void test_mem() {
-
-	byte b0 = 0x0a; // пишем байт , читаем байт
-	byte b1 = 0x0b;
-	word w = 0x0b0a;
-
-	//ТЕСТ№1
-	w_write (0, w);//записываем слово
-	word wres = w_read(0);//читаем слово 
-	printf("%04hx = %04hx\n",w, wres ); // hhx - half half hex (4) 
-	//%02hhx - ширина 2 с ведущими нулями
-	assert(w == wres); // чтобы в мае гулять на природе и шашлыки жарить
-						// ну или ботать матан 
-	
-	//ТЕСТ№2 читаем слово пишем два байта
-
-	Adress a = 4;
-	w_write(a, w);
-	word b0res = b_read(a);
-	word b1res = b_read(a+1);
-	printf("ww/br \t %04hx = %02hhx%02hhx\n", wres, b1res, b0res );
-	assert (w == wres);
-
-
-	//ТЕСТ№3 проверка функции b_write
-
-	word w0 = 0x0b0a;
-	word w1 = 0x0c0c;
-	byte b = 0x0c;
-	w_write(4, w0);
-
-	b_write(4,b);
-	b_write(5,b);
-	word wres1 = w_read(4);//читаем слово  
-	printf("wo/bb \t %04hx = %02hhx%02hhx\n", wres1, b, b);
-	assert(w1 == wres1);
-}
-*/
-void load_file(const char * file_name);
-
-int main (int argc, char * argv[]) {
-	//printf("%d, %s\n", argc, argv[0] );
-	b_write(ostat, 0200);
-	load_file(argv[1]);
-	run();
-	//test_mem();
-	return 0;
-}
-
-const Command cmd[] = {
+word mem[MEMSIZE];
+word reg[8]; //регистры R0 .. R7
+int trc = 0;
+byte N = 0, Z = 0, C = 0;
+byte it_is_byte = 0;
+int nn = 0;
+char xx = 0;
+int Rnn = 0;
+struct mr ss = {}, dd = {};
+const struct Command cmd[] = {
 	{0177777, 0000000, "HALT",	do_halt,	NO_PARAM},
 	{0170000, 0060000, "ADD",	do_add,		HAS_SS|HAS_DD}, 
 	{0170000, 0010000, "MOV",	do_mov,		HAS_SS|HAS_DD},
@@ -74,6 +31,41 @@ const Command cmd[] = {
 	{0170000, 0120000, "CMPB",	do_cmpb,	HAS_SS|HAS_DD},
 	{0000000, 0000000, "UNKNOWN", do_unknown,   NO_PARAM}
 };
+
+int main (int argc, char * argv[]) {
+	//printf("%d, %s\n", argc, argv[0] );
+	b_write(ostat, 0200);
+
+	if ((argc < 2) || (argc > 3)) {
+		printf("\nExample: ./My_pdp [trace flags] (file)\n\
+\n\
+noflag\t== 60no tracing\n\
+-t\t== little tracing\n\
+-T\t== big tracing\n\n\
+");
+		return 0;
+	}
+
+	printf("======================== running ==============================\n");
+	if (argc == 2){
+		trc = 0;
+		load_file(argv[1]);
+		run();
+	}
+
+	if (argc == 3){
+		if (!strcmp("-t", argv[1]))
+			trc = 1;
+		if (!strcmp("-T", argv[1]))
+			trc = 2;
+		load_file(argv[2]);
+		run();
+	}
+
+
+
+	return 0;
+}
 
 void load_file(const char * file_name){
 	FILE * f_inp = fopen(file_name, "rb");
@@ -125,4 +117,41 @@ void w_write (Adress adr, word w) {
 	mem[adr] = w;
 }
 
+/*void test_mem() {
 
+	byte b0 = 0x0a; // пишем байт , читаем байт
+	byte b1 = 0x0b;
+	word w = 0x0b0a;
+
+	//ТЕСТ№1
+	w_write (0, w);//записываем слово
+	word wres = w_read(0);//читаем слово 
+	printf("%04hx = %04hx\n",w, wres ); // hhx - half half hex (4) 
+	//%02hhx - ширина 2 с ведущими нулями
+	assert(w == wres); // чтобы в мае гулять на природе и шашлыки жарить
+						// ну или ботать матан 
+	
+	//ТЕСТ№2 читаем слово пишем два байта
+
+	Adress a = 4;
+	w_write(a, w);
+	word b0res = b_read(a);
+	word b1res = b_read(a+1);
+	printf("ww/br \t %04hx = %02hhx%02hhx\n", wres, b1res, b0res );
+	assert (w == wres);
+
+
+	//ТЕСТ№3 проверка функции b_write
+
+	word w0 = 0x0b0a;
+	word w1 = 0x0c0c;
+	byte b = 0x0c;
+	w_write(4, w0);
+
+	b_write(4,b);
+	b_write(5,b);
+	word wres1 = w_read(4);//читаем слово  
+	printf("wo/bb \t %04hx = %02hhx%02hhx\n", wres1, b, b);
+	assert(w1 == wres1);
+}
+*/
